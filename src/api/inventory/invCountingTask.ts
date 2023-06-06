d795340be0b0d978ab4e88188e8cbdb971b21ec7
xiumubai <1547702880@qq.com>
2023-06-06 10:43:43 +0800

    feat: 盘点任务

--- src/api/inventory/invCountingTask.ts
+++ src/api/inventory/invCountingTask.ts
@@ -2,7 +2,7 @@
  * @Author: 朽木白
  * @Date: 2023-06-05 10:17:03
  * @LastEditors: 1547702880@@qq.com
- * @LastEditTime: 2023-06-06 16:59:57
+ * @LastEditTime: 2023-06-06 17:02:28
  * @Description: 实时库存
  */
 
